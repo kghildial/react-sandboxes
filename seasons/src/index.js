9c39914efe6a35b3dc183c6bd5f6f5cd0c857d32
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2019-02-19 11:12:29 +0530

    Add seasons app

---
+++ seasons/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+    state = {
+        lat: null,
+        errMessage: ''
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }), 
+            err => this.setState({ errMessage: err.message })
+        );
+    }
+
+    // Helper method for rendering on terms of conditional logic
+    renderContent() {
+        if (this.state.errMessage && !this.state.lat) {
+            return <div>Error: {this.state.errMessage}</div>;
+        }
+
+        if (!this.state.errMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+
+        return <Spinner message="Please accept location request" />
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
